d86fa2bed33f5652b3245eab584571c9639bff49
Rohith Daka <rohith.daka@gmail.com>
2015-04-04 09:48:25 -0700

    Spectrum Analyzer can show the probed signals
    Finally got the spectrum Analyzer to show the data properly from each of
    the probes.
    
    Still has to program the mixer output and filter output probes.

--- js/rf.js
+++ js/rf.js
@@ -1,5 +1,5 @@
 // Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
     width = 500 - margin.left - margin.right,
     height = 180 - margin.top - margin.bottom;
 
@@ -17,7 +17,7 @@ var yAxis = d3.svg.axis().scale(y)
 // Define the line
 var valueline = d3.svg.line()
     .x(function(d) { return x(d.freq); })
-    .y(function(d) { return y(d.mag1); });
+    .y(function(d) { return y(d.magn1); });
     
 // Adds the svg canvas
 var svg = d3.select("#spectrumAnalyzer")
@@ -27,31 +27,173 @@ var svg = d3.select("#spectrumAnalyzer")
             .attr("transform", 
                 "translate(" + margin.left + "," + margin.top + ")");
 
+//Lookout for these variable
+var NofSamples = 200;
+var i;
+var data_antenna=[]; 
+var data_amplifier=[]; 
+var data_mixer=[]; 
+var data_lo=[];
+var data_filter=[];
+
 // Get the data
+var data_total;
 d3.csv("js/noiseSignal.csv", function(error, data) {
+    
     data.forEach(function(d) {
         d.freq = +d.freq;
-        d.mag = +d.mag1;
+        d.magn1 = +d.magn1;
+        d.magn2 = +d.magn2;
+        d.mag1 = +d.mag1;
+        d.mag2 = +d.mag2;
+        d.mag3 = +d.mag3;
+        d.mag4 = +d.mag4;
+        d.mag5 = +d.mag5;
     });
-
+    data_total = data;
+    
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.freq; }));
     y.domain([0, 15]);
-
-    // Add the valueline path.
-    svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(data));
-
+    
     // Add the X Axis
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
-
+    
     // Add the Y Axis
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
 
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
 });
+
+    //Update function which updates the data based on a hidden radio checked button 
+
+    //Checking hidden radio buttons to preserve the Probe selection so that the user can have the flexibility to click elsewhere (esp on spectrum Analyzer)
+
+    document.getElementById("antennaOut").onclick = 
+        function() {
+            document.getElementById("antenna_check").checked=true;
+            updateData();
+            drawGraph();
+        };
+
+    document.getElementById("amplifierOut").onclick =
+        function() {
+            document.getElementById("amplifier_check").checked=true;
+            updateData();
+            drawGraph();
+    };
+
+    document.getElementById("loOut").onclick =
+        function () {
+            document.getElementById("lo_check").checked=true;
+            updateData();
+            drawGraph();
+        };
+
+    document.getElementById("mixerOut").onclick = 
+        function (){ 
+            document.getElementById("mixer_check").checked=true;
+            updateData();
+            drawGraph();
+        };
+
+    document.getElementById("filterOut").onclick =
+        function () {
+            document.getElementById("filter_check").checked=true;
+            updateData();
+            drawGraph();
+        };
+
+    document.getElementById("CF").oninput = 
+        function (){
+            updateData();
+            drawGraph();
+    };
+    document.getElementById("LO").oninput = 
+        function (){
+            updateData();
+            drawGraph();
+    };
+
+
+    //
+    function updateData() {    
+
+    //Calculate signal at Antenna Output based on Carrier Frequency
+
+        var CFF = +document.getElementById("CF").value;
+        var LOF = +document.getElementById("LO").value;
+        var pie = Math.PI;
+        console.log(CFF);
+
+        for(i=0; i <=NofSamples;i++){
+            if (i < CFF -4 || i > CFF + 4){
+                data_antenna[i] =0;
+            }else{
+                data_antenna[i] = 5*(Math.sin(pie/2 + (i - CFF)*pie/8));
+            }
+            data_total[i].mag1 = data_antenna[i] + ((Math.random() < 0.5)? data_total[i].magn1 : data_total[i].magn2 ) ;
+            data_total[i].mag2= 2*data_total[i].mag1;
+            if ( i != LOF ) {
+                data_lo[i] = (Math.random() < 0.5)? data_total[i].magn1 : data_total[i].magn2;
+            }else {
+                data_lo[i]= 5 + ((Math.random() < 0.5)? data_total[i].magn1 : data_total[i].magn2);
+            }
+            data_total[i].mag3 = data_lo[i];
+        }
+
+
+    }
+
+
+    function drawGraph() {    
+
+        // Scale the range of the data again 
+            x.domain(d3.extent(data_total, function(d) { return d.freq; }));
+            y.domain([0, 15]);
+
+        // Select the section we want to apply our changes to
+            var svg = d3.select("#spectrumAnalyzer").transition();
+
+        var selectedProbe = document.getElementsByName("selectedProbe");
+        if (selectedProbe[0].checked){
+            valueline = d3.svg.line()
+                .x(function(d) { return x(d.freq); })
+                .y(function(d) { return y(d.mag1); });
+        }else if(selectedProbe[1].checked){
+            valueline = d3.svg.line()
+                .x(function(d) { return x(d.freq); })
+                .y(function(d) { return y(d.mag2); });
+        }else if(selectedProbe[2].checked){
+            valueline = d3.svg.line()
+                .x(function(d) { return x(d.freq); })
+                .y(function(d) { return y(d.mag3); });        
+        }else if(selectedProbe[3].checked){
+            valueline = d3.svg.line()
+                .x(function(d) { return x(d.freq); })
+                .y(function(d) { return y(d.mag4); });
+        }else if(selectedProbe[4].checked){
+            valueline = d3.svg.line()
+                .x(function(d) { return x(d.freq); })
+                .y(function(d) { return y(d.mag5); });
+        }
+        
+        
+
+        // Make the changes
+            svg.select(".line")   // change the line
+                .duration(750)
+                .attr("d", valueline(data_total));
+    }
+
+
+
+
